2c49f62904c739aab81031d4d89298af566fab13
Deni <deni.munjas@gmail.com>
2016-04-11 00:15:23 +0200

    Spinner for registering suggestion

---
+++ client/modules/core/components/private/shared/loading/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <div className="leftEye"></div>
+      <div className="rightEye"></div>
+      <div className="mouth"></div>
+    </div>
+  );
+}
