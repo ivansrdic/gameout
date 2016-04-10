784fc87f2128ca00e19cb8116ae2d0e96a72f096
Deni <deni.munjas@gmail.com>
2016-04-11 01:33:14 +0200

    Added undo changes to customize character.
    Also customize character is no longer a form since no data is really submited to be validated and pushed to database. In the future this can be refactored.

--- client/modules/core/components/private/profile-setup/customize-character.jsx
+++ client/modules/core/components/private/profile-setup/customize-character.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {Grid, Row, Col, Panel, Input, ButtonGroup, ButtonInput} from 'react-bootstrap';
+import {Grid, Row, Col, Panel, Input, ButtonGroup, ButtonInput, Button} from 'react-bootstrap';
 import Character from '../shared/character/character.jsx';
 import SkinDescriptionList from '../shared/character-sprites/skin-description-list.jsx';
 
 export default class CustomizeCharacter extends Component {
   constructor(props) {
     super(props);
+    this.lastAppearanceIds = this.props.getAppearanceIds();
   }
 
   render() {
@@ -16,43 +17,41 @@ export default class CustomizeCharacter extends Component {
       equipSkin
     } = this.props;
 
-    console.log(character, "DENI OVO JE KARAKTER");
+    console.log(character, "Current character");
 
     return (
       <Col md={10} mdOffset={1}>
         <h1 className="text-center">Customize character</h1>
-        <form onSubmit={(e) => this.handleFromSubmit(e)}>
-          <label htmlFor="level" className="control-label input-group">Gender</label>
-          <ButtonGroup ref="gender" className="form-group" bsSize="large" data-toggle="buttons">
-            <label onClick={e => this.handleGenderPick(e, "male")} className="btn btn-default active">
-              <input name="gender" value="male" type="radio"/>Male
-            </label>
-            <label onClick={e => this.handleGenderPick(e, "female")} className="btn btn-default">
-              <input name="gender" value="female" type="radio"/>Female
-            </label>
-          </ButtonGroup>
+        <label htmlFor="level" className="control-label input-group">Gender</label>
+        <ButtonGroup ref="gender" className="form-group" bsSize="large" data-toggle="buttons">
+          <label onClick={e => this.handleGenderPick(e, "male")} className="btn btn-default active">
+            <input name="gender" value="male" type="radio"/>Male
+          </label>
+          <label onClick={e => this.handleGenderPick(e, "female")} className="btn btn-default">
+            <input name="gender" value="female" type="radio"/>Female
+          </label>
+        </ButtonGroup>
 
-          <Row>
-            <Col sm={6} lg={6}>
-              <label htmlFor="level" className="control-label input-group">Character</label>
-              <div className="character-container">
-                <Character character={character} isNaked={true}/>
-              </div>
-            </Col>
-            <Col sm={6} lg={6}>
-              <label htmlFor="level" className="control-label input-group">Hair/Torso/Legs/Skin</label>
-              <div className="inventory">
-                <SkinDescriptionList
-                  getSkins={getSkins}
-                  getAppearanceIds={getAppearanceIds}
-                  equipItem={equipSkin}
-                />
-              </div>
-            </Col>
-          </Row>
+        <Row>
+          <Col sm={6} lg={6}>
+            <label htmlFor="level" className="control-label input-group">Character</label>
+            <div className="character-container">
+              <Character character={character} isNaked={true}/>
+            </div>
+          </Col>
+          <Col sm={6} lg={6}>
+            <label htmlFor="level" className="control-label input-group">Hair/Torso/Legs/Skin</label>
+            <div className="inventory">
+              <SkinDescriptionList
+                getSkins={getSkins}
+                getAppearanceIds={getAppearanceIds}
+                equipItem={equipSkin}
+              />
+            </div>
+          </Col>
+        </Row>
 
-          <ButtonInput className="pull-right" type="submit" value="Save"/>
-        </form>
+        <Button onClick={this.handleUndoClick.bind(this)} className="pull-right">Undo Changes</Button>
 
       </Col>
     );
@@ -63,7 +62,11 @@ export default class CustomizeCharacter extends Component {
     //  One day soon enough when we get a female model.
   }
 
-  handleFromSubmit(e) {
-    e.preventDefault();
+  handleUndoClick(e) {
+    const {equipSkin, getAppearanceIds} = this.props;
+    const currentAppearanceIds = getAppearanceIds();
+
+    for (let i = 0; i < this.lastAppearanceIds.length; i++)
+      if (this.lastAppearanceIds[i] != currentAppearanceIds[i]) equipSkin(this.lastAppearanceIds[i]);
   }
 }
