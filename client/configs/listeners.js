b9eec56bde780aecd139f738d204b9d83eeb21a4
Ivan Srdić <ivansrdic93@gmail.com>
2016-05-05 15:38:21 +0200

    Continued work on database listeners.

--- client/configs/listeners.js
+++ client/configs/listeners.js
@@ -1,12 +1,14 @@
 import {Characters} from '/collections';
 import _ from 'lodash';
+
 export default ({LocalState}) => {
 
   LocalState.set('changesMessage', []);
-  let count = 0;
 
   const characterCollection = Characters.find({ownerId: Meteor.userId()});
   let character = null;
+
+  const messagePipe = new ChangesMessagePipe(LocalState);
 
   characterCollection.observeChanges({
     added(id, fields) {
@@ -15,25 +17,75 @@ export default ({LocalState}) => {
     },
 
     changed(id, fields) {
-      let state = LocalState.get('changesMessage');
+      messagePipe.getState();
 
-      _.forOwn(fields.stats, function(stat, key) {
-        const value = stat - character.stats[key];
+      if(fields.stats) {
+        _.forOwn(fields.stats, function(stat, key) {
+          const value = stat - character.stats[key];
 
-        if(value != 0) {
-          state = _.concat(state, {key: count, name: key, value});
-          LocalState.set('changesMessage', state);
+          if(value != 0) {
+            messagePipe.add({name: key, value});
+          }
+        });
+      }
 
-          count++;
+      if(fields.equipment) {
+        const oldFields = _.keys(character.equipment);
+        const newFields = _.keys(fields.equipment);
 
-          setTimeout(function () {
-            const state = LocalState.get('changesMessage');
-            LocalState.set('changesMessage', _.drop(state));
-          }, 5000);
-        }
-      });
+        console.log(_.difference(oldFields, newFields));
+        console.log(_.difference(newFields, oldFields));
+
+        /*if(oldFields.length < newFields.length) {
+          messagePipe.add({name: "Equipped ", value: _.difference(oldFields, newFields)});
+        } else if(oldFields.length > newFields.length) {
+          messagePipe.add({name: "Unequipped ", value: _.difference(oldFields, newFields)});
+        } else {
+          messagePipe.add({name: "Replaced ", value: _.difference(oldFields, newFields)});
+        }*/
+      }
+
+
 
       character = Characters.findOne(id);
     }
   });
+}
+
+class ChangesMessagePipe {
+  constructor(LocalState) {
+    this.pipe = [];
+    this.LocalState = LocalState;
+    this.count = 0;
+  }
+
+  getState() {
+    this.pipe = this.LocalState.get('changesMessage');
+  }
+
+  add(element) {
+    element.key = this.count;
+
+    this.pipe = _.concat(this.pipe, element);
+    this.set();
+
+    this.count++;
+
+    this.timeoutRemove();
+  }
+
+  remove() {
+    this.pipe = _.drop(this.LocalState.get('changesMessage'));
+    this.set();
+  }
+
+  set() {
+    this.LocalState.set('changesMessage', this.pipe);
+  }
+
+  timeoutRemove(time = 5000) {
+    setTimeout(function() {
+      this.remove();
+    }.bind(this), time);
+  }
 }
