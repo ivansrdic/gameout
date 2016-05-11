0b538315ba3f58b4b0883849aa643a3b376ad2d8
Deni <deni.munjas@gmail.com>
2016-05-11 02:14:28 +0200

    Reducing InfoBar height.
    This is easily git revertable in case it turns out undesirable.

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -36,7 +36,7 @@ class MainLayout extends Component {
         <div>
           <Navigation user={this.props.user} logout={this.props.logout} private={true}/>
           <ChangesMessage></ChangesMessage>
-          <div style={{marginTop: 140 + "px"}}>
+          <div style={{marginTop: 100 + "px"}}>
             {this.props.content(this.props.user)}
           </div>
           <Footer />
